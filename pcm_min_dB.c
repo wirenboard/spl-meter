1753f49cac34540ba0f847a21e9268fd8d9d5985
Evgeny Boger <boger@contactless.ru>
2014-07-03 05:40:43 +0400

    fix format

--- pcm_min_dB.c
+++ pcm_min_dB.c
@@ -3,20 +3,20 @@
 
 static char *device = "default";	/* device name */
 
-/* 
-   Each sample is a 16-bit signed integer, which means 
+/*
+   Each sample is a 16-bit signed integer, which means
    the value can vary from -32768 to 32767.
 
-   Size of the sample buffer = 8*1024 = 8192, which takes 
+   Size of the sample buffer = 8*1024 = 8192, which takes
    8*1024*2 = 16384 bytes.
 
    Sampling frequency = 48000Hz, in other words, sound
    card will record the sample value every 1/48000 second.
 */
-short buffer[8*1024];			
+short buffer[8*1024];
 
 /*
-   The unit of sizeof() function is byte, so we have to 
+   The unit of sizeof() function is byte, so we have to
    shift 1 bit to the right to calculate the real size of buffer.
 */
 int buffer_size = sizeof(buffer)>>1;
@@ -31,26 +31,26 @@ double rms(short *buffer)
 	long int square_sum = 0.0;
 	for(i=0; i<buffer_size; i++)
 		square_sum += (buffer[i] * buffer[i]);
-	
+
 	double result = sqrt(square_sum/buffer_size);
 	return result;
 }
-	
+
 int main(void)
 {
 	int err;
 	unsigned int i;
-	
+
 	snd_pcm_t *handle_capture;	/* handle of capture */
 
 	snd_pcm_sframes_t frames;
-	
+
 	// Open handle of capture
 	if((err=snd_pcm_open(&handle_capture, device, SND_PCM_STREAM_CAPTURE, 0)) < 0) {
 		printf("Capture open error: %s\n", snd_strerror(err));
 		exit(EXIT_FAILURE);
 	}
-	
+
 	if((err = snd_pcm_set_params(handle_capture,
 				     SND_PCM_FORMAT_S16_LE,
 				     SND_PCM_ACCESS_RW_INTERLEAVED,
@@ -63,10 +63,10 @@ int main(void)
 	}
 	printf("             ");
 	fflush(stdout);
-	
+
 	/*
 	 * Formula of dB is 20log((Sound Pressure)/P0)
-	   Assume that (Sound Pressure/P0) = k * sample value (Linear!),	   
+	   Assume that (Sound Pressure/P0) = k * sample value (Linear!),
 	   and by experiment, we found that k = 0.45255.
 	*/
 	double k = 0.45255;
@@ -75,7 +75,7 @@ int main(void)
 	int peak = 0;
 	i=0;
 
-	// Capture 
+	// Capture
 	while(i<50) {
 		frames = snd_pcm_readi(handle_capture, buffer, buffer_size);
 		if(frames < 0)
@@ -87,15 +87,15 @@ int main(void)
 			printf("Short read (expected %li, wrote %li)\n", (long)buffer_size, frames);
 
 		Pvalue = rms(buffer) * k;
-		
+
 		dB = (int)20*log10(Pvalue);
 		if(dB > peak)
 			peak = dB;
-		int j;	
-		for(j=0; j<13; j++)
+		int j;
+		for(j=0; j<20; j++)
 			printf("\b");
 		fflush(stdout);
-		printf("dB=%d,Peak=%d", dB, peak);
+		printf("dB=%4d,Peak=%4d", dB, peak);
 		fflush(stdout);
 	}
 	printf("\n");
